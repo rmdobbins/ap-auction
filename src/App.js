c83b17c2bc70b266676dc9eb9b81587be554d613
Russell Dobbins <russell.dobbins@yahoo.com>
2023-09-02 08:06:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+toimport logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. You save and reload!
         </p>
         <a
           className="App-link"
